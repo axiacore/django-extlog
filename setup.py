e2eeafc6df6a9ba7c4ee905720853311e1f950f6
ctrl-alt-delete <ctrl-alt-delete@live.ru>
2014-07-31 08:41:28 -0500

    master  fix path in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -14,7 +11,6 @@ setup(
     include_package_data=True,
     license='MIT',
     description='Extended log for Django, that tracks changes in models.',
-    long_description=README,
     url='https://github.com/AxiaCore/django-extlog',
     author='Vera Mazhuga',
     author_email='ctrl-alt-delete@live.ru',
