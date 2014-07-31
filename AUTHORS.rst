4c42575fcf936c53f1051e1a0827c0dbf1988ea3
ctrl-alt-delete <ctrl-alt-delete@live.ru>
2014-07-31 08:34:29 -0500

    master  licence and setup files

---
+++ AUTHORS.rst
@@ -0,0 +1,5 @@
+Primary Author(s):
+
+ * Vera Mazhuga (https://github.com/ctrl-alt-delete)
+
+Other Contributors:
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -14,7 +14,7 @@ setup(
     include_package_data=True,
     license='MIT',
     description='Extended log for Django, that tracks changes in models.',
-    long_description=open('README.rst', 'r').read(),
+    long_description=README,
     url='https://github.com/AxiaCore/django-extlog',
     author='Vera Mazhuga',
     author_email='ctrl-alt-delete@live.ru',
