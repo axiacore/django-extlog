a8f6710577f64a1e4f873ec52cbb1cd1ff9578d3
ctrl-alt-delete <ctrl-alt-delete@live.ru>
2014-07-31 09:33:47 -0500

    master  change package name

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-extlog',
-    version='0.2',
-    packages=['extlog'],
+    version='0.3',
+    packages=['django_extlog'],
     include_package_data=True,
     license='MIT',
     description='Extended log for Django, that tracks changes in models.',
