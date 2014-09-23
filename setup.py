c66e0af868b1fed369ecb9cf2f4791e310da9e8e
ctrl-alt-delete <ctrl-alt-delete@live.ru>
2014-09-23 14:45:38 -0500

    master  change version to 0.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-extlog',
-    version='0.7',
+    version='0.8',
     packages=['django_extlog'],
     include_package_data=True,
     license='MIT',
